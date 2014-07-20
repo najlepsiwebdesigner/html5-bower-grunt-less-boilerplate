6ea7fc229f1066b000357c6a5d469e0876c4cf5c
Peter Beňo <najlepsiwebdesigner@gmail.com>
2014-07-20 21:12:37 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["./bower_components/bootstrap/less"]
+                },
+                files: {
+                    "./css/style.css": "./css/style.less"
+                }
+            }
+        },
+        cssmin: {
+            'dist': {
+                'src': 'css/style.css',
+                'dest': 'css/style.min.css'
+            }
+        },        
+        watch: {
+            files: "./css/style.less",
+            tasks: ["less", "cssmin"]
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-yui-compressor');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
